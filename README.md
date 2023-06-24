d76a348f3d12adc90719e4dc32bef83947d5a177
KANCHARLA VAMSIKRISHNA PRASAD <65493037+vamsilearner@users.noreply.github.com>
2023-06-24 21:37:33 +0530

    vite-reactts-app

---
+++ README.md
@@ -0,0 +1,6 @@
+# npm init vite@latest boat-website 
+# selct - react - ts - 
+# npm i
+# npm run dev - to run
+# npm i -D vite-plugin-windicss windicss - to run windicss
+# npm i framer-motion // tranforms html elements to motion elements
---
+++ src/Navbar.tsx
@@ -0,0 +1,55 @@
+import Logo from "./Logo";
+import ArrowRight from "./ArrowRight";
+import { motion } from "framer-motion";
+import { animationStart, reveal } from "./utils/animation";
+
+function Navbar() {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5, delay: animationStart }}
+    >
+      <motion.div
+        variants={reveal}
+        initial="hiddenVariant"
+        animate="revealedVariant"
+        transition={{
+          ease: "easeIn",
+          type: "tween",
+          staggerChildren: 0.1,
+          duration: 0.5,
+          delayChildren: animationStart + 0.5,
+        }}
+        className="w-full flex items-center justify-around h-80px fixed top-0 bg-white z-100"
+      >
+        <motion.div variants={reveal}>
+          <Logo />
+        </motion.div>
+        <div className="flex gap-20px items-center <md:hidden">
+          <motion.span variants={reveal} className="cursor-pointer">
+            Home.
+          </motion.span>
+          <motion.span variants={reveal} className="cursor-pointer">
+            Experience.
+          </motion.span>
+          <motion.span variants={reveal} className="cursor-pointer">
+            Portfolio.
+          </motion.span>
+          <motion.span variants={reveal} className="cursor-pointer">
+            Skills.
+          </motion.span>
+        </div>
+        <motion.div
+          variants={reveal}
+          className="flex gap-5px items-center cursor-pointer"
+        >
+          <span className="mb-3px">sign in</span>
+          <ArrowRight />
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+}
+
+export default Navbar;
